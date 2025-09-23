2e4a2c8d8706a02ed97160da75f42a3a0fa01619
Ryan Torres <ryantorres77690@gmail.com>
2025-09-23 11:13:27 -0300

    criando arquivos

--- ScreenSound_API/Program.cs
+++ ScreenSound_API/Program.cs
@@ -10,11 +10,31 @@ using (HttpClient client = new HttpClient())
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
 
 
-        musicas[1].ExibirDetalhesDaMusica();
-        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
-        LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
-        LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
-        LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+        //musicas[1].ExibirDetalhesDaMusica();
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+
+
+        //var musicasPreferidasDoDaniel = new MusicasPreferidas("Daniel");
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[377]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[4]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[6]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1467]);
+
+        //var musicasPreferidasEmilly = new MusicasPreferidas("Emy");
+
+        //musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[500]);
+        //musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[637]);
+        //musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[428]);
+        //musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[13]);
+        //musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[71]);
+
+        //musicasPreferidasEmilly.ExibirMusicasFavoritas();
+
+        //musicasPreferidasEmilly.GerarArquivoJson();
 
 
     }
