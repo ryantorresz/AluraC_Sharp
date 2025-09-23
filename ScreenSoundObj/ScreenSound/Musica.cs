3a3cc072a6c4ef3f9645a954571e1228d41159e1
Ryan Torres <ryantorres77690@gmail.com>
2025-09-23 07:56:29 -0300

    nova classe avaliação

--- ScreenSoundObj/ScreenSound/Musica.cs
+++ ScreenSoundObj/ScreenSound/Musica.cs
@@ -1,23 +1,19 @@
-﻿namespace ScreenSound.Modelos;
-
-class Musica
+﻿class Musica
 {
-    public Musica(Banda artista, string nome)
-    {
-        Artista = artista;
-        Nome = nome;
-    }
-
-    public string Nome { get; }
-    public Banda Artista { get; }
+    public string Nome { get; set; }
+    public string Artista { get; set; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
-    public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}";
+    public string DescricaoResumida => 
+        $"A música {Nome} pertence à banda {Artista}";
+
+
+
 
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {Nome}");
-        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Artista: {Artista}");
         Console.WriteLine($"Duração: {Duracao}");
         if (Disponivel)
         {
