fe55d8a93bebb3f8a00c7aaa75ecf0525bebb347
Ryan Torres <ryantorres77690@gmail.com>
2025-09-24 10:53:44 -0300

    Parse e StreamWriter

--- CsharpArquivos/ByteBankIO/Program.cs
+++ CsharpArquivos/ByteBankIO/Program.cs
@@ -1,21 +1,10 @@
-﻿partial class Program
+﻿using ByteBankIO;
+
+partial class Program
 {
     static void Main(string[] args)
     {
-        var enderecoDoArquivo = "contas.txt";
-        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-        {
-            var leitor = new StreamReader(fluxoDoArquivo);
-            // var linha = leitor.ReadLine();
-            // var texto = leitor.ReadToEnd();
-            // var numero = leitor.Read();
-
-            while (!leitor.EndOfStream)
-            {
-                var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
-            }
-        }
+        CriarArquivoComWriter();   
         Console.ReadLine();
     }
 }
