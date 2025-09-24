167798984ad4a7edd672e4c2a0e512e220f15c98
Ryan Torres <ryantorres77690@gmail.com>
2025-09-24 10:30:34 -0300

    FileStream e leitor StreamReader

--- CsharpArquivos/ByteBankIO/Program.cs
+++ CsharpArquivos/ByteBankIO/Program.cs
@@ -1,36 +1,21 @@
-﻿using ByteBankIO;
-using System.Text;
-
-class Program
+﻿partial class Program
 {
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open, FileAccess.Read);
-
-        var buffer = new byte[1024]; // 1KB
-
-        while (numeroDeBytesLidos != 0)
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
+            var leitor = new StreamReader(fluxoDoArquivo);
+            // var linha = leitor.ReadLine();
+            // var texto = leitor.ReadToEnd();
+            // var numero = leitor.Read();
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
         }
-        // public override int Read(byte[] array, int offset, int count);
         Console.ReadLine();
-    }
-
-    static void EscreverBuffer(byte[] buffer)
-    {
-        var utf8 = new UTF8Encoding();
-
-        var texto = utf8.GetString(buffer);
-        Console.Write(texto);
-
-        //foreach (var meuByte in buffer)
-        //{
-        //    Console.Write(meuByte);
-        //    Console.Write(" ");
-        //}
     }
 }
