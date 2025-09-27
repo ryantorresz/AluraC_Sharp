9f1bedc0f7fc1f564994a8257ccec0d5ad9b5f75
Ryan Torres <ryantorres77690@gmail.com>
2025-09-27 12:54:15 -0300

    Nível de visibilidade'

--- curso_bibliotecas/bytebank_ATENDIMENTO/Program.cs
+++ curso_bibliotecas/bytebank_ATENDIMENTO/Program.cs
@@ -1,5 +1,24 @@
-﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+﻿using bytebank.Modelos.ADM.Funcionarios;
+using bytebank_ATENDIMENTO.bytebank.Atendimento;
+
+
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 new ByteBankAtendimento().AtendimentoCliente();
 
+//public class Estagiario : Funcionario
+//{
+//    public Estagiario(double salario, string cpf) : base(salario, cpf)
+//    {
+//    }
 
+//    public override void AumentarSalario()
+//    {
+//        // implementação;
+//    }
+
+//    protected override double getBonificacao()
+//    {
+//        // implementação;
+//        return 0;
+//    }
+//}
